c8433ca00c5531e39cc6b39ca84a480fb0da2b44
Tyler Zeller <tylermzeller@gmail.com>
2017-04-10 14:05:18 -0400

    Create python package

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 Author: Tyler Zeller
 
-A fully functional puzzle cube written in Python. 
+A fully functional puzzle cube written in Python 2.7.10.
+
+Download with:
+
+```
+pip install cubey
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cubey',
+      version='1.0',
+      description='A fully functional puzzle cube written in python.',
+      url='http://github.com/tylermzeller/cubey',
+      author='Tyler Zeller',
+      author_email='tylermzeller@gmail.com',
+      license='MIT',
+      packages=['cubey'],
+      zip_safe=False)
